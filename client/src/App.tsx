df9a0c9152bf9a5b1579c2af5176371f3c83d2bb
Corned <Corned@users.noreply.github.com>
2024-08-04 22:17:51 +0300

    markdown scss

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,30 +1,14 @@
-import { BarChartIcon, BinaryIcon, BookAIcon, DiffIcon, GitBranchIcon, GithubIcon, GitlabIcon, GraduationCapIcon, HomeIcon, HouseIcon, LogOutIcon, MenuSquareIcon, SchoolIcon, SettingsIcon } from "lucide-react"
+import { BarChartIcon, BinaryIcon, BookAIcon, DiffIcon, GitBranchIcon, GithubIcon, GitlabIcon, GraduationCapIcon, HomeIcon, HouseIcon, LogOutIcon, MenuIcon, MenuSquareIcon, SchoolIcon, SettingsIcon } from "lucide-react"
+import Markdown from "react-markdown"
 
-
-import logo from "./assets/digitalents-logo.jpg"
-
-const Collapsable = () => {
-  return (
-    <div>
-
-    </div>
-  )
-}
-
-const NavItem = ({ icon, label }) => {
-  return (
-    <div className="nav__item">
-      { icon }
-      { label }
-    </div>
-  )
-}
 
 const Nav = () => {
   return (
     <div className="nav">
 
-      <div className="nav__spacer"></div>
+      <div className="nav__item nav__item--narrow ml-auto">
+        <MenuIcon />
+      </div>
 
       <div className="nav__header">
         <p>Digitalents Academy</p>
@@ -61,10 +45,28 @@ const Nav = () => {
   )
 }
 
-const Content = () => {
-  return (
-    <div className="content">
 
+
+const Content = () => {
+
+  const md = `
+# Welcome to Digitalents Academy!
+
+Hello! This is the home page of Digitalents Academy, the new learning platform for x, y and z.
+
+This platform allows you to:
+
+* Learn the basics of HTML, CSS and JavaScript
+  * Learn the basics of HTML, CSS and JavaScript
+    * Learn the basics of HTML, CSS and JavaScript
+      * Learn the basics of HTML, CSS and JavaScript
+* Get into developing Single Page Applications with React.js
+`
+
+  return (
+    <div className="content markdown">
+      {/* <p className='text-7xl font-bold'>Hello John!</p> */}
+      <Markdown>{ md }</Markdown>
     </div>
   )
 }
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom/client"
 
 import "./globals.css"
 import "./index.css"
+import './markdown.scss'
 
-import { Badge } from "./components/ui/badge"
 import App from "./App"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
