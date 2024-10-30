b673dbea0bdd619a436229a57437879ecdc06ad7
corned <oliver.kaarnalehto@gmail.com>
2024-10-30 13:12:23 +0200

    basic routing

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,12 +3,20 @@ import ReactDOM from "react-dom/client"
 
 import "./globals.css"
 import "./index.css"
-import './markdown.scss'
+import "./markdown.scss"
 
 import App from "./App"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+])
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
