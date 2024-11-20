b5ab7f1e0757c69e5f418cf2e0922dca1c43252f
corned <oliver.kaarnalehto@gmail.com>
2024-11-20 14:39:27 +0200

    fix

--- server/src/middleware/requireAuth.ts
+++ server/src/middleware/requireAuth.ts
@@ -2,10 +2,10 @@ import { type Request, type Response, type NextFunction } from "express"
 
 const requireAuth = (req: Request, res: Response, next: NextFunction) => {
   if (!res.locals.userId) {
-    return res.status(401).json({ error: "Unauthorized" })
+    res.status(401).json({ error: "Unauthorized" })
+  } else {
+    next()
   }
-
-  next()
 }
 
 export default requireAuth
